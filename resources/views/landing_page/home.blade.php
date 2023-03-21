ae90e82e4561de631535eb713fa6608cf85631e0
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-03-21 17:00:21 +0800

    added some image

--- resources/views/landing_page/home.blade.php
+++ resources/views/landing_page/home.blade.php
@@ -5,12 +5,19 @@
 @endsection
 
 @section('content')
-    <section id="landingHome" class="py-5 text-center">
+    <section id="landingHome" class="py-5 bg-light">
         <div class="container">
-            <h1>Welcome to My Landing Page</h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod bibendum tortor,
-                vel tincidunt quam ullamcorper eu.</p>
-            <a href="#about" class="btn btn-primary btn-lg mt-3">Learn More</a>
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <h1>Welcome to AGri-Map</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod bibendum
+                        tortor, vel tincidunt quam ullamcorper eu.</p>
+                    <a href="#about" class="btn btn-primary btn-lg mt-3">Learn More</a>
+                </div>
+                <div class="col-md-6">
+                    <img src="{{ asset('location.png') }}" alt="" class="img-fluid">
+                </div>
+            </div>
         </div>
     </section>
 
@@ -18,24 +25,24 @@
         <div class="container">
             <h2 class="text-center text-uppercase mb-5">About Us</h2>
             <div class="row">
-                <div class="col-md-4 text-center">
-                    <i class="fa fa-map fa-4x mb-5"></i>
+                <div class="col-md-4 text-center mt-3">
+                    <i class="fa fa-map fa-4x mb-5 text-primary"></i>
                     <h3>Accurate Soil Analysis</h3>
                     <p class="text-justify">Our soil mapping system is designed to help farmers and agricultural
                         professionals accurately analyze
                         the health and quality of their soil. By providing detailed soil maps and data, we empower our users
                         to make informed decisions about their crops and land management practices.</p>
                 </div>
-                <div class="col-md-4 text-center">
-                    <i class="fa fa-wifi fa-4x mb-5"></i>
+                <div class="col-md-4 text-center mt-3">
+                    <i class="fa fa-wifi fa-4x mb-5 text-warning"></i>
                     <h3>Advanced Technology</h3>
                     <p class="text-justify">Our system uses state-of-the-art technology to collect and analyze soil data,
                         including satellite
                         imagery and ground-based sensors. Our algorithms are designed to provide high-resolution maps of
                         soil properties such as nutrient levels, pH, and moisture content.</p>
                 </div>
-                <div class="col-md-4 text-center">
-                    <i class="fa fa-leaf fa-4x mb-5"></i>
+                <div class="col-md-4 text-center mt-3">
+                    <i class="fa fa-leaf fa-4x mb-5 text-success"></i>
                     <h3>Sustainable Agriculture</h3>
                     <p class="text-justify">We believe that sustainable agriculture starts with a deep understanding of the
                         soil. Our system is
@@ -51,11 +58,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <img src="https://via.placeholder.com/600x400" alt="Soil Parameters Image" class="img-fluid">
+                    <div id="mapid" style="height: 400px;"></div>
                 </div>
                 <div class="col-md-6">
-                    <h2>Soil Parameters</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis arcu et turpis scelerisque
+                    <h2 class="text-center">Soil Parameters</h2>
+                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis arcu et turpis
+                        scelerisque
                         auctor. Curabitur volutpat eget felis id maximus. Donec id velit dolor.</p>
                 </div>
             </div>
@@ -65,7 +73,7 @@
     <section id="contact" class="py-5">
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-8 p-4">
                     <h2>Contact Us</h2>
                     <form>
                         <div class="form-group">
@@ -83,10 +91,30 @@
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
-                <div class="col-md-6">
-                    <img src="https://via.placeholder.com/600x400" alt="Contact Us Image" class="img-fluid">
+                <div class="col-md-4">
+                    <img src="{{ asset('images/chat.png') }}" alt="Contact Us Image" class="img-fluid">
                 </div>
             </div>
         </div>
     </section>
 @endsection
+
+@push('scripts')
+    @livewireScripts
+    <script>
+        // initialize map and set its center and zoom level
+        var map = L.map('mapid').setView([12.668945714230706, 123.88067528173328], 18);
+
+        // add tile layer to map
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            maxZoom: 18,
+            tileSize: 512,
+            zoomOffset: -1
+        }).addTo(map);
+
+        // add marker to map
+        L.marker([12.668945714230706, 123.88067528173328]).addTo(map)
+            .bindPopup("<b>Bulan National High School!</b><br />Lorem Ipsum.").openPopup();
+    </script>
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>@yield('title')</title>
     <meta name="description" content="Agri-Map" />
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('location.png') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="canonical" href="url" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
@@ -15,6 +15,10 @@
     <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    {{-- Leaflet --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/custom/leaflet/leaflet.bundle.css') }}">
+
     <style>
         [x-cloak] {
             display: none !important;
@@ -53,9 +57,10 @@
             min-height: calc(100vh - 60px);
         }
 
-        /* body {
-            height: 90vh;
-        } */
+        #mapid {
+            height: 400px;
+        }
+
         #landingHome {
             padding-top: 80px;
             display: flex;
@@ -72,7 +77,11 @@
     <nav class="navbar navbar-expand-md navbar-dark fixed-top" data-spy="affix" data-offset-top="100"
         data-offset-bottom="100" <div class="container-fluid ms-5 me-5">
         <a class="navbar-brand mb-auto" href="#">
-            <h1 class="text-success">Agri-<span class="text-warning">Map</span></h1>
+            <div style="display: flex; align-items: center;">
+                <img src="{{ asset('location.png') }}" alt="" width="30" height="24"
+                    class="d-inline-block align-text-top">
+                <h1 class="text-success m-0"> Agri-<span class="text-warning">Map</span></h1>
+            </div>
         </a>
         <button class="navbar-toggler text-warning" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
@@ -103,6 +112,67 @@
 
     @section('content')@show
 
+    <script>
+        var KTAppSettings = {
+            "breakpoints": {
+                "sm": 576,
+                "md": 768,
+                "lg": 992,
+                "xl": 1200,
+                "xxl": 1400
+            },
+            "colors": {
+                "theme": {
+                    "base": {
+                        "white": "#ffffff",
+                        "primary": "#3699FF",
+                        "secondary": "#E5EAEE",
+                        "success": "#1BC5BD",
+                        "info": "#8950FC",
+                        "warning": "#FFA800",
+                        "danger": "#F64E60",
+                        "light": "#E4E6EF",
+                        "dark": "#181C32"
+                    },
+                    "light": {
+                        "white": "#ffffff",
+                        "primary": "#E1F0FF",
+                        "secondary": "#EBEDF3",
+                        "success": "#C9F7F5",
+                        "info": "#EEE5FF",
+                        "warning": "#FFF4DE",
+                        "danger": "#FFE2E5",
+                        "light": "#F3F6F9",
+                        "dark": "#D6D6E0"
+                    },
+                    "inverse": {
+                        "white": "#ffffff",
+                        "primary": "#ffffff",
+                        "secondary": "#3F4254",
+                        "success": "#ffffff",
+                        "info": "#ffffff",
+                        "warning": "#ffffff",
+                        "danger": "#ffffff",
+                        "light": "#464E5F",
+                        "dark": "#ffffff"
+                    }
+                },
+                "gray": {
+                    "gray-100": "#F3F6F9",
+                    "gray-200": "#EBEDF3",
+                    "gray-300": "#E4E6EF",
+                    "gray-400": "#D1D3E0",
+                    "gray-500": "#B5B5C3",
+                    "gray-600": "#7E8299",
+                    "gray-700": "#5E6278",
+                    "gray-800": "#3F4254",
+                    "gray-900": "#181C32"
+                }
+            },
+            "font-family": "Poppins"
+        };
+    </script>
+    <script src="{{ asset('assets/plugins/custom/leaflet/leaflet.bundle.js') }}"></script>
     <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
     <script src="{{ asset('assets/plugins/custom/prismjs/prismjs.bundle.js') }}"></script>
     <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
@@ -114,7 +184,6 @@
 
     @stack('scripts')
     <script src="{{ mix('js/app.js') }}"></script>
-
 </body>
 
 </html>
