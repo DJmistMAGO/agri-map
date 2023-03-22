2b381f9776fec59aa7c7a4161319f6ac057bfa8d
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-03-22 12:02:04 +0800

    add cropped image

--- resources/views/landing_page/home.blade.php
+++ resources/views/landing_page/home.blade.php
@@ -5,15 +5,14 @@
 @endsection
 
 @section('content')
-
     <nav class="navbar navbar-expand-md navbar-dark fixed-top" data-spy="affix" data-offset-top="100" data-offset-bottom="100"
         id="navbar">
         <div class="container-fluid ms-5 me-5">
             <a class="navbar-brand mb-auto" href="#">
                 <div style="display: flex; align-items: center;">
-                     <img src="{{ asset('images/7.png') }}" alt=""
-                    class="d-inline-block align-text-top max-h-40px">
-                    <h1 class="text-success m-0"> Agri-<span class="text-warning">Map</span></h1>
+                    <img src="{{ asset('images/7.png') }}" alt=""
+                        class="d-inline-block align-text-top max-h-40px m-0">
+                    {{-- <h1 class="text-success m-0"> Agri-<span class="text-warning">Map</span></h1> --}}
                 </div>
             </a>
             <button class="navbar-toggler text-warning" type="button" data-bs-toggle="collapse"
@@ -47,16 +46,18 @@
         <div class="container">
             <div class="row align-items-center">
 
-                <div class="col-md-6" data-aos="fade-right" data-aos-duration="1500">
-                    <h1 class="h1">Welcome to AGri-Map</h1>
-                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod bibendum
-                        tortor, vel tincidunt quam ullamcorper eu.</p>
+                <div class="col-md-5" data-aos="fade-right" data-aos-duration="1500">
+                    <img src="{{ asset('images/7.5.png') }}" alt="agri-map logo" class="img-fluid p-4 ml-auto">
+                    <h1>Digital Mapping System</h1>
+                    <p class="h4">For the Municipality of Bulan</p>
                     <a href="#about" class="btn btn-primary btn-lg mt-3">Learn More</a>
                 </div>
+                <div class="col-md-3">
 
-                <div class="col-md-6" data-aos="fade-left" data-aos-duration="1500">
-                    <img src="{{ asset('location.png') }}" alt="" class="img-fluid p-4">
-
+                </div>
+                <div class="col-md-4" data-aos="fade-left" data-aos-duration="1500"
+                    style="border-radius: 50%; background-color:white; display: inline-block; padding: 10px;">
+                    <img src="{{ asset('images/5.5.png') }}" alt="agrimap" class="img-fluid p-5">
                 </div>
             </div>
         </div>
@@ -72,7 +73,8 @@
                         <h3 class="mb-5 mt-5">Accurate Soil Analysis</h3>
                         <p class="text-justify">Our soil mapping system is designed to help farmers and agricultural
                             professionals accurately analyze
-                            the health and quality of their soil. By providing detailed soil maps and data, we empower our users
+                            the health and quality of their soil. By providing detailed soil maps and data, we empower our
+                            users
                             to make informed decisions about their crops and land management practices.</p>
                     </div>
                 </div>
@@ -80,7 +82,8 @@
                     <div class="card card-custom px-8 py-8"style="height: 360px;">
                         <i class="fa fa-wifi fa-4x mb-5 text-warning"></i>
                         <h3 class="mb-5 mt-5">Advanced Technology</h3>
-                        <p class="text-justify">Our system uses state-of-the-art technology to collect and analyze soil data,
+                        <p class="text-justify">Our system uses state-of-the-art technology to collect and analyze soil
+                            data,
                             including satellite
                             imagery and ground-based sensors. Our algorithms are designed to provide high-resolution maps of
                             soil properties such as nutrient levels, pH, and moisture content.</p>
@@ -90,10 +93,12 @@
                     <div class="card card-custom px-8 py-8" style="height: 360px; "">
                         <i class="fa fa-leaf fa-4x mb-5 text-success"></i>
                         <h3 class="mb-5 mt-5">Sustainable Agriculture</h3>
-                        <p class="text-justify">We believe that sustainable agriculture starts with a deep understanding of the
+                        <p class="text-justify">We believe that sustainable agriculture starts with a deep understanding of
+                            the
                             soil. Our system is
                             designed to help farmers and land managers optimize their use of resources, reduce waste, and
-                            improve crop yields. With our soil mapping system, you can take control of your soil health and make
+                            improve crop yields. With our soil mapping system, you can take control of your soil health and
+                            make
                             data-driven decisions for the future of your farm or land.</p>
                     </div>
 
